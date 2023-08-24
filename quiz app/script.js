e1e9a9cbb90150e03fe8efe46161d0856904799c
lyomu <gmnyomu@gmail.com>
2023-08-24 12:22:18 +0300

    Weather app v2

--- quiz app/script.js
+++ quiz app/script.js
@@ -71,9 +71,34 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === 'true';
     if (isCorrect){
         selectedBtn.classList.add('correct');
+        score++;
     }
     else{
         selectedBtn.classList.add('incorrect');
     }
+    Array.from(answerButton.children).forEach (button => {
+        if(button.dataset.correct=== 'true'){
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display= 'block';
 }
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener('Click', ()=>{
+    if(currentQuestionIndex < questions.length ){
+        handleNextButton();
+    } else{
+        startQuiz();
+    }
+} )
  startQuiz();
