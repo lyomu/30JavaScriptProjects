5b3ba9ab01e0ec3b993f84a7568d338d36d9676b
lyomu <gmnyomu@gmail.com>
2023-08-30 00:10:58 +0300

    Project complete - execCommand

--- password-generator/script.js
+++ password-generator/script.js
@@ -7,6 +7,8 @@ const lowerCase= 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
 const symbol = '@#$%^&*()_+~|}{[]<>/-=';
 
+const allChars= upperCase + lowerCase+number+symbol;
+
 function createPassword(){
     let password = " ";
     password += upperCase[Math.floor(Math.random()* upperCase.length)];
@@ -14,4 +16,14 @@ function createPassword(){
     password += number[Math.floor(Math.random()* number.length)];
     password += symbol[Math.floor(Math.random()* symbol.length)];
     
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword (){
+    passwordBox.select();
+    document.execCommand('copy');
 }
